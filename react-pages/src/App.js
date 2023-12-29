db1671c97701c4ec00e13c37cec143daeaf61d0e
Q219057 <daiunii@naver.com>
2023-12-29 16:08:17 +0900

    react update

--- react-pages/src/App.js
+++ react-pages/src/App.js
@@ -1,18 +1,27 @@
+/*
 import logo from './logo.svg';
 import './App.css';
 import LoginForm from './loginForm';
+*/
+import './css/template.css';
+import Info from './template/info';
+import Img from './template/img';
+import Text from './template/text';
+import Calender from './template/calender';
+import Map from './template/map';
+import Gallery from './template/gallery';
+import Attention from './template/attention';
+import Comment from './template/comment';
 
 function App() {
   const name = 'HyunYoung';
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi,{name}!
-        </p>
-        <LoginForm />
       </header>
+      <div className="invitation_con">
+      <Info />
+      </div>
     </div>
   );
 }
---
+++ react-pages/src/template/attention.js
@@ -0,0 +1,11 @@
+function Info() {
+  return (
+    <div className="head_con">
+        <div className="head_txt">최현영 & 최현영</div>
+        <div className="head_txt">2023년 12월 22일</div>
+        <div className="head_txt">코엑스 2층 아셈볼룸</div>
+    </div>
+  );
+}
+
+export default Info;
